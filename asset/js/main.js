8554fa7c38eb14b3a977c2a528b51cb7d8b8b4f6
FabioVasi <vasifabio96@gmail.com>
2023-08-22 12:48:16 +0200

    fix

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,4 @@
-//Anni: chiediamo all'utente la sua età e comunichiamo l'anno di nascita.
+
 
 /*
 
@@ -13,16 +13,46 @@ Strumenti.
 
 // Chiedere età utente
 
-let userAge = prompt('type your age');
+//let userAge = prompt('type your age');
 
 // recupero l'anno corrente con una const
 
-const currentYear = 2023;
+//const currentYear = 2023;
 
 // sottraggo all'anno corrente gli anni dell'utente
 
-const birthYear = currentYear - Number(userAge);
+//const birthYear = currentYear - Number(userAge);
 
 // stampo in console il risultato
 
-console.log('ciao sei nato il ' + birthYear);
+//console.log('ciao sei nato il ' + birthYear);
+
+
+
+//Salutare: chiediamo all'utente il suo nome e inseriamo in pagina la stringa "ciao <nome_utente>"
+
+
+//Strumenti.
+/*
+- prompt
+- document.getElementById
+- console.log
+
+*/
+
+//chiedere il nome utente
+
+const userName = prompt('inserisci il tuo nome');
+
+//selezionare il tag tramite id in cui inserire il risultato
+
+
+const gredingsElement = document.getElementById('gredings');
+console.log(gredingsElement);
+//stampare il risultato in console
+
+console.log('Ciao' + userName);
+
+// stampare il risultato in pagina
+
+gredingsElement.innerHTML = 'Ciao ' + userName; 
