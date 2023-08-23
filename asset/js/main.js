4aa0259f42e742555708c27bcd7d68b9ae2d9e38
FabioVasi <vasifabio96@gmail.com>
2023-08-23 10:03:56 +0200

    correction

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,6 @@
-//Anni: chiediamo all'utente la sua età e comunichiamo l'anno di nascita.
-
 /*
+
+Anni: chiediamo all'utente la sua età e comunichiamo l'anno di nascita.
 
 Strumenti.
 
@@ -9,51 +9,95 @@ Strumenti.
 - consoleLog
 - document.getElementById
 
+Chiedere età utente
+
+let userAge = prompt('type your age');
+
+recupero l'anno corrente con una const
+
+const currentYear = 2023;
+
+sottraggo all'anno corrente gli anni dell'utente
+
+const birthYear = currentYear - Number(userAge);
+
+stampo in console il risultato
+
+console.log('ciao sei nato il ' + birthYear);
+
 */
 
-// Chiedere età utente
 
-//let userAge = prompt('type your age');
-
-// recupero l'anno corrente con una const
-
-//const currentYear = 2023;
-
-// sottraggo all'anno corrente gli anni dell'utente
-
-//const birthYear = currentYear - Number(userAge);
-
-// stampo in console il risultato
-
-//console.log('ciao sei nato il ' + birthYear);
-
-
-
-//Salutare: chiediamo all'utente il suo nome e inseriamo in pagina la stringa "ciao <nome_utente>"
-
-//Strumenti.
 
 /*
+
+Salutare: chiediamo all'utente il suo nome e inseriamo in pagina la stringa "ciao <nome_utente>"
+
+Strumenti.
 
 - prompt
 - document.getElementById
 - console.log
 
-*/
-
-//chiedere il nome utente
+chiedere il nome utente
 
 const userName = prompt('inserisci il tuo nome');
 
-//selezionare il tag tramite id in cui inserire il risultato
+selezionare il tag tramite id in cui inserire il risultato
 
 const greetingsElement = document.getElementById('greetings');
 console.log(greetingsElement);
 
-//stampare il risultato in console
+stampare il risultato in console
 
 console.log('Ciao' + userName);
 
-// stampare il risultato in pagina
+ stampare il risultato in pagina
 
 greetingsElement.innerHTML = 'Ciao ' + userName;
+
+*/
+
+
+
+/*
+
+Es.s n°1 JS
+
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato,
+che va formattato in questo modo: nomecognomecolorepreferito23
+
+Strumenti.
+
+- prompt
+- Let/const
+- document.getElementById
+- console.log
+- 
+
+const userName = prompt('type your name here');
+
+const userSurname = prompt('type your surname here');
+
+const favouriteColor = prompt('type your favourite color here');
+
+console.log(userName, userSurname, favouriteColor);
+
+const result = `${userName + userSurname + favouriteColor}23`;
+
+console.log(result);
+
+const passwordElement = document.getElementById('password');
+console.log(passwordElement);
+
+passwordElement.innerHTML = result;
+
+*/
+
+
+
+
+
+
+
+
